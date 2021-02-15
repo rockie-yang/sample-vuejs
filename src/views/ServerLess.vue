eeca2580ae4566dd47fc87e0f11282fff924b6c3
Rockie Yang <youjiang.yang@tele2.com>
2021-02-15 15:46:29 +0100

    add serverless

--- src/views/ServerLess.vue
+++ src/views/ServerLess.vue
@@ -2,16 +2,17 @@
   <div>
     <b-container>
       <b-row>
-        <b-col xs-6>
+        <b-col xs-2>
           <span class="label">Environment: </span>
           <span>
             <b-form-select
+              label="Environment:"
               v-model="environment"
               :options="environments"
             ></b-form-select>
           </span>
         </b-col>
-        <b-col xs-6>
+        <b-col xs-2>
           <span class="label">Runtime: </span>
           <span>
             <b-form-select
@@ -20,11 +21,47 @@
             ></b-form-select>
           </span>
         </b-col>
+        <b-col xs-3>
+          <span class="label">Functions: </span>
+          <span>
+            <b-form-select
+              v-model="functionName"
+              :options="functionNames"
+            ></b-form-select>
+          </span>
+        </b-col>
+        <b-col xs-5 mt-5 lg-5>
+          <div class="btn-group" role="group">
+            <button type="button" class="btn btn-secondary">Modify</button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="deleteFunction"
+            >
+              Delete
+            </button>
+          </div>
+          <b-input-group prepend="Name:">
+            <b-form-input v-model="newFunctionName"></b-form-input>
+            <b-button variant="outline-success" @click="createFunction"
+              >Create</b-button
+            >
+          </b-input-group>
+          <b-input-group prepend="data:">
+            <b-form-input v-model="data"></b-form-input>
+            <b-button variant="outline-success" @click="invokeFunction"
+              >Invoke</b-button
+            >
+          </b-input-group>
+        </b-col>
       </b-row>
       <b-row class="h-100">
         <b-col xs-12 ms-12 lg-12>
           <codemirror v-model="code" :options="cmOptions" />
         </b-col>
+      </b-row>
+      <b-row>
+        <div>{{ result }}</div>
       </b-row>
     </b-container>
   </div>
@@ -42,15 +79,20 @@ import "codemirror/mode/python/python.js";
 
 // import theme style
 import "codemirror/theme/base16-light.css";
-
+import axios from "axios";
 export default {
   components: { codemirror },
   data() {
     return {
       environment: "AWS",
+      newFunctionName: "just-a-test",
       environments: ["AWS", "Azure", "On Premisise"],
-      runtime: "Python 3.8",
-      runtimes: ["Python 3.8", "Node 15.8"],
+      runtime: "python3.7",
+      runtimes: ["python3.7", "node15.8"],
+      functionName: "just-a-test",
+      functionNames: ["just-a-test"],
+      data: "hello world",
+      result: "",
       cmOptions: {
         tabSize: 4,
         mode: "python",
@@ -59,17 +101,60 @@ export default {
         // line: true
         // more CodeMirror options...
       },
-      code: `import json
-import boto3
-
-region = 'eu-west-1'
-instances = ['i-0b16548546fdebe0c']
-ec2 = boto3.client('ec2', region_name=region)
-
-def lambda_handler(event, context):
-    ec2.stop_instances(InstanceIds=instances)
-    print(f'Stopped your instances {instances}')`,
+      code: `def lambda_handler(event, context):
+  print(event)
+  return event['data']`,
     };
+  },
+  methods: {
+    // deleteFunction() {
+    //   console.log(`delete function ${this.func} in ${this.environment}`);
+    // },
+    deleteFunction() {
+      const url = `http://157.230.251.182:5000/function/${this.functionName}`;
+      console.log(`Deleting function ${url}`);
+      axios
+        .delete(url)
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+    },
+
+    invokeFunction() {
+      const url = `http://157.230.251.182:5000/call?env=${this.environment}&name=${this.functionName}`;
+      console.log(`Invoking ${url} with data ${this.data}`);
+      axios
+        .post(url, this.data, { headers: { "Content-Type": "text/plain" } })
+        .then((res) => {
+          this.ret = res.data["result"].replace("b", "", 1);
+          this.result = res.data;
+        })
+        .catch((err) => console.log(err));
+    },
+
+    createFunction() {
+      const url = `http://157.230.251.182:5000/function?env=${this.environment}&runtime=${this.runtime}&name=${this.newFunctionName}`;
+      console.log(url);
+      axios
+        .post(url, this.code, { headers: { "Content-Type": "text/plain" } })
+        .then((res) => {
+          console.log(res.data);
+          this.result = res.data;
+          this.functionNames.push(this.newFunctionName);
+        })
+        .catch((err) => console.log(err));
+    },
+    async getFunctions() {
+      const res = await axios.get("http://157.230.251.182:5000/function");
+      return res.data;
+    },
+  },
+  mounted() {
+    // this.functionNames = this.getFunctions();
+    // console.log(this.functionNames);
+    axios
+      .get("http://157.230.251.182:5000/function")
+      .then((res) => (this.functionNames = res.data.map((obj) => obj["name"])))
+      .catch((err) => console.log(err));
   },
   props: ["hostDNS"],
 };
